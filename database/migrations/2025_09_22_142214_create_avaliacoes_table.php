8873ab92673803801ada63ccafba37e5da454825
WereHound <murakami_gabriel@yahoo.com>
2025-09-22 18:11:46 -0300

    avaliacao Tabela

--- database/migrations/2025_09_22_142214_create_avaliacoes_table.php
+++ database/migrations/2025_09_22_142214_create_avaliacoes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,14 @@ return new class extends Migration
         Schema::create('avaliacoes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('nota');
+            $table->text('comentario');
+            $table->string('estado_equipamento');
+            $table->string('cumprimento_contrato');
+
+            $table->foreignId('locacao_id')->constrained('locacao')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('locadores_id')->constrained('locatariosdalocacao')->onDelete('restrict')->onUpdate('restrict');
+
         });
     }
 
