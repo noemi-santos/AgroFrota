daa6f4fdd528e5bab6ffefc369e28801f377237f
italo91206 <italoferreiradjk14@gmail.com>
2025-09-22 11:38:46 -0300

    fix: better database migration files

---
+++ database/migrations/2025_09_22_142119_create_equipamentos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipamento', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->integer('ano');
+            $table->string('capacidade');
+            $table->float('preco_periodo');
+            $table->string('disponibilidade_calendario');
+            $table->float('raio_atendimento');
+            $table->boolean('exige_operador_certificado');
+            $table->boolean('seguro_obrigatorio');
+            $table->boolean('caucao_obrigatoria');
+
+            // Foreign Keys
+            $table->foreignId('locador_id')->constrained('locador')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('categoria_id')->constrained('categoria')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipamento');
+    }
+};
