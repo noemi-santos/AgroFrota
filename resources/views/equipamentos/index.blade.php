c35451ba4d71c314cbff9b4f7bdfae90f99a3c77
WereHound <murakami_gabriel@yahoo.com>
2025-09-24 16:28:16 -0300

    removal of extra info
    also changed the acoes buttons class from d-flex gap-2 to btn-group

--- resources/views/equipamentos/index.blade.php
+++ resources/views/equipamentos/index.blade.php
@@ -12,31 +12,34 @@
     <a href="/equipamentos/create" class="btn btn-success mb-3">Novo Registro</a>
     <table class="table table-hover table-striped">
         <thead>
-            <tr>
-                <th>ID</th>
+            <tr >
+                <th>Categoria</th>
                 <th>Nome</th>
                 <th>Marca</th>
-                <th>modelo</th>
+                <th>Modelo</th>
+                <th>Preço</th>
                 <th>Locador</th>
-                <th>Categoria</th>
-                <th>Ano</th>
-                <th>Capacidade</th>
-                <th>Preço/Período</th>
-                <th>Disponibilidade</th>
-                <th>Raio</th>
-                <th>Operador Cert.</th>
-                <th>Seguro</th>
-                <th>Caução</th>
+                <th class="text-center">Operador Cert.</th>
+                <th class="text-center">Seguro</th>
+                <th class="text-center">Caução</th>
                 <th>Ações</th>
             </tr>
         </thead>
         <tbody>
             @foreach($equipamentos as $e)
-                <tr class="text-center">
-                    <td>{{ $e->id }}</td>
+                <tr class="align-middle">
+                    <td>
+                        @foreach ($categorias as $c)
+                        @if ($e->categoria_id == $c->id)
+                        {{ $c->titulo }}
+                        @endif
+                        @endforeach
+                    </td>
                     <td>{{ $e->nome }}</td>
                     <td>{{ $e->marca }}</td>
                     <td>{{ $e->modelo }}</td>
+                    
+                    <td>{{ $e->preco_periodo }}</td>
                     <td>
                         @foreach ($locador as $l)
                             @if ($e->locador_id == $l->id)
@@ -44,42 +47,32 @@
                             @endif
                         @endforeach
                     </td>
-                    <td>
-                        @foreach ($categorias as $c)
-                            @if ($e->categoria_id == $c->id)
-                                {{ $c->titulo }}
-                            @endif
-                        @endforeach
-                    </td>
-                    <td>{{ $e->ano }}</td>
-                    <td>{{ $e->capacidade }}</td>
-                    <td>{{ $e->preco_periodo }}</td>
-                    <td>{{ $e->disponibilidade_calendario }}</td>
-                    <td>{{ $e->raio_atendimento }}</td>
-                    <td>
+                    <td class="text-center">
                         @if($e->exige_operador_certificado == 0)
                             <img src="{{ asset('images/xmark.svg') }}" width="20rem">
                         @else
                             <img src="{{ asset('images/checkmark.svg') }}" width="20rem">
                         @endif
                     </td>
-                    <td>
+                    <td class="text-center">
                         @if($e->seguro_obrigatorio == 0)
                             <img src="{{ asset('images/xmark.svg') }}" width="20rem">
                         @else
                             <img src="{{ asset('images/checkmark.svg') }}" width="20rem">
                         @endif
                     </td>
-                    <td>
+                    <td class="text-center">
                         @if($e->caucao_obrigatoria == 0)
                             <img src="{{ asset('images/xmark.svg') }}" width="20rem">
                         @else
                             <img src="{{ asset('images/checkmark.svg') }}" width="20rem">
                         @endif
                     </td>
-                    <td class="d-flex gap-2">
-                        <a href="/equipamentos/{{ $e->id }}/edit" class="btn btn-sm btn-warning">Editar</a>
-                        <a href="/equipamentos/{{ $e->id }}" class="btn btn-sm btn-info">Consultar</a>
+                    <td >
+                        <div class="btn-group">
+                            <a href="/equipamentos/{{ $e->id }}/edit" class="btn btn-sm btn-warning">Editar</a>
+                            <a href="/equipamentos/{{ $e->id }}" class="btn btn-sm btn-info">Consultar</a>
+                        </div>
                     </td>
                 </tr>
             @endforeach
