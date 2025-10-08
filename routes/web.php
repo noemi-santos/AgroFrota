b60251ed5346caedfb298caa71e72dc3f05ab68d
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-10-07 21:55:41 -0300

    routes middleware

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,12 @@ Route::post("/login", [AuthController::class, "Login"]);
 Route::get("/cadastrar", [AuthController::class], "ShowFormCadastro");
 Route::post("/cadastrar", [AuthController::class], "CadastrarUsuario");
 
-
+Route::middleware("auth")->group(function (){
+    Route::resource('equipamentos', EquipamentoController::class);
+    Route::resource('categorias', CategoriaController::class);
+    Route::resource('locador', LocadorController::class);
+    Route::resource('locatario', LocatarioController::class);
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::post("/logout", [AuthController::class], "Logout");
+    Route::get("/inicial", function() { return view("inicial"); });
+});
