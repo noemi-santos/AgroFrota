1e45ba2ba3389c07f4e09968d0cf2a95515d0297
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-10-07 21:49:02 -0300

    routes for login and cadastrar

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\LocatarioController;
 use App\Http\Controllers\LocadorController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/', function () {
@@ -17,3 +18,10 @@ Route::resource('categorias', CategoriaController::class);
 Route::resource('locador', LocadorController::class);
 Route::resource('locatario', LocatarioController::class);
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get("/login", [AuthController::class, "ShowFormLogin"])->name("login");
+Route::post("/login", [AuthController::class, "Login"]);
+Route::get("/cadastrar", [AuthController::class], "ShowFormCadastro");
+Route::post("/cadastrar", [AuthController::class], "CadastrarUsuario");
+
+
