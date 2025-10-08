b4c65ee4d85ef29c63e766ff2bd6cae468b0c396
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-10-07 21:39:29 -0300

    Auth Controller Base

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function ShowFormLogin(){
+        return view("login");
+    }
+
+    public function ShowFormCadastro()
+    {
+        return view("cadastro");
+    }
+    
+    public function CadastrarUsuario( Request $request )
+    {
+        try{
+            $dados = $request->all();
+            $dados["password"] = Hash::make($dados["password"]);
+            User::create($dados);
+            return redirect()->route("login")->with("Sucesso", "Novo usuario registrado!");
+        } 
+        catch(Exception $e){
+            Log::error(
+            "Erro ao criar o usuario: ". $e->getMessage(), 
+            [
+                "stack" => $e->getTraceAsString(),
+                "request" => $request->all()
+            ]);
+        }
+    }
+
+    public function Login( Request $request)
+    {
+        $credentials = $request->only("email", "password");
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->route("inicial");
+        }
+        else{
+            return redirect()->route("login")->with("erro", "credenciais invalidas");
+        }
+    }
+
+    public function Logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route("login");
+    }
+}
