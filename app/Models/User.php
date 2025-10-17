a3c8c42508527588ed819f3017e14c7ab68b547a
WereHound <murakami_gabriel@yahoo.com>
2025-10-17 14:05:11 -0300

    Models
    deletion of models and additions to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        
+        'telefone',
+        'endereco',
+        'cnpj_cpf',
+        'documentos_validados',
+        'reputacao_media',
     ];
 
     /**
